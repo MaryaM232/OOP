73c903108051ea6803c63b964b9a7441e55910a0
MaryaM232 <mamonovamariya@mail.ru>
2022-11-15 21:51:14 +0300

    New code

--- src/Witch.java
+++ src/Witch.java
@@ -2,7 +2,7 @@ import java.util.List;
 public class Witch extends BaseHero{
     private boolean magic;
     public Witch(List<BaseHero> band, int x, int y) {
-        super(String.format("Magician#%d", ++Witch.number), 30, 17, 12, 0, new int[]{-5, 0}, 9);
+        super(String.format("Witch#%d", ++Witch.number), 30, 17, 12, 0, new int[]{-5, 0}, 9);
         magic = true;
         position = new Vector2(x, y);
         super.setBand(band);
